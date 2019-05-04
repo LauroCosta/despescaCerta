be80b32a7e1b9843769cd0859dc92c7b6417e3fc
LauroCosta <joaolauro94@gmail.com>
2019-05-03 21:41:11 -0300

    crud biometria

--- backend/src/controllers/BiometryController.js
+++ backend/src/controllers/BiometryController.js
@@ -3,25 +3,34 @@ const Biometry = require('../models/Biometry');
 class BiometryController {
 
     async store (req, res){
-
-        const biometry = await Biometry.create(
-            { 
-               nurseryNumber: 2, 
-               shirimpCount: 40,  
-               sampleWeight: 2000,  
-               grammage: 3 
-            }
-        );
+        const biometry = await Biometry.create(req.body);
         
         return res.json(biometry);
-
-    }
+    },
 
     async show(req, res) {
-
         const biometry = await Biometry.findById(req.params.id);
+        
         return res.json(biometry);
+    },
+
+    async update(req, res) {
+        const biometry = await Biometry.findByIdAndUpdate(req.params.id, req.body,{
+            new: true
+        })
+
+        return res.json(biometry);
+    },
+
+    
+    async destroy(req, res){
+
+        await Biometry.findByIdAndRemove(req.params.id);
+
+        return res.send();
     }
+
+
 
 }
 
